099beed6cf000554d1e57a19b9d891fcd5046ab8
atmoner <contact.atmoner@gmail.com>
2023-04-05 12:00:35 +0200

    Add template for actions!

--- cosmos.config.js
+++ cosmos.config.js
@@ -9,8 +9,10 @@ export default [
       viewDenom: 'BCNA',
       chainDenom: 'ubcna',
       addressPrefix: 'bcna',
-      icon: `https://raw.githubusercontent.com/osmosis-labs/assetlists/main/images/bcna.svg`,
+      icon: `https://raw.githubusercontent.com/cosmos/chain-registry/master/bitcanna/images/bcna.svg`,
     },
+    feeMultiplier: 1.3,
+    gasPrice: 0.025,
     coingeckoId: 'bitcanna',
     nftContract: 'stars1cpzvvmlrc9lcw3q5yrznax0uk5h6xww2d4ch9xve4xu8mfvgw2kqepnwd2',
     sg721: 'stars1w4dff5myjyzymk8tkpjrzj6gnv352hcdpt2dszweqnff927a9xmqc7e0gv'
