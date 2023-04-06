7a5f3175c5ba98273c429f5f7cebe293f24635a9
atmoner <contact.atmoner@gmail.com>
2023-04-06 22:52:21 +0200

    Update proposal creation page

--- pages/create-proposal/index.vue
+++ pages/create-proposal/index.vue
@@ -1,262 +1,74 @@
 <template>
-
-    <v-row justify="space-around">
-    <v-col>
-  <v-card>
-    <v-tabs
-      v-model="tab"
- 
-      centered
-      dark
-      icons-and-text
+  <v-row align="center" justify="center" style="height:100vh" dense>
+    <v-col cols="12" lg="6" md="6" class=" fill-height d-flex flex-column align-center">
+      <v-alert
+      v-if="isSend"
+      dense
+      text
+      color="#00b786"
+      type="success"
+      width="600"
     >
-      <v-tabs-slider></v-tabs-slider>
-
-      <v-tab href="#tab-1">
-        V1beta1
-      </v-tab>
-
-      <v-tab href="#tab-2">
-        v1
-      </v-tab>
- 
-    </v-tabs>
-
-    <v-tabs-items v-model="tab">
-      <v-tab-item value="tab-1" >
-        <v-card flat >
-          <v-card-text>
-          
-          
- 
-      <v-card  class="accent justify-center" max-width="600">
+      Your proposal has been created! <strong>Click here</strong> to see the proposal
+    </v-alert>
+      <v-card class="accent" max-width="600">
         <v-card-title>
-          <span class="text-h5">Create v1beta1 proposal</span>
+          <span class="text-h5">Create text proposal (v1beta)</span>
         </v-card-title>
         <v-card-text>
           <v-container>
             <v-row>
-              <v-col
-                cols="12"
-                sm="12"
-              >
-                <v-select
-                  :items="items"
-                  label="Proposal type"
-                  required
-                ></v-select>
-              </v-col>            
-              <v-col
-                cols="12"
-                sm="6"
-                md="6"
-              >
+             <!--  <v-col cols="12" sm="12">
+                <v-select :items="items" label="Proposal type" required></v-select>
+              </v-col> -->
+              <v-col cols="12" sm="12">
                 <v-text-field
-                  label="Proposer"
+                  v-model="propTitle"
+                  label="Proposal title" 
+                  outlined
                   required
                 ></v-text-field>
               </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-                md="6"
-              >
-                <v-text-field
-                  label="initialDeposit" 
-                ></v-text-field>
+              <v-col cols="12" sm="12">
+                <v-textarea
+                  v-model="propText"
+                  outlined
+                  name="input-7-4"
+                  label="Proposal text"
+                  required
+                ></v-textarea>
               </v-col>
- 
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field v-model="proposer" outlined label="Proposer" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field v-model="initDeposit" outlined label="Initial Deposit (ubcna)" required></v-text-field>
+              </v-col>
 
- 
             </v-row>
           </v-container>
-          <small>*indicates required field</small>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
-            Close
-          </v-btn>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
-            Save
+
+          <v-btn color="#00b786" @click="createProposalv1Beta">
+            Submit proposal
           </v-btn>
         </v-card-actions>
-      </v-card>  
- 
-      
-      
-      
-      
-          </v-card-text>
-        </v-card>
-      </v-tab-item>
-      <v-tab-item value="tab-2">
-        <v-card flat>
-          <v-card-text>
-      <v-col>
-      <!--{{ infoGroupId }}-->
+      </v-card>
+    </v-col>
 
-        <br />
-        <v-row>
-          <v-col cols="6">
-            <v-card
-            height="500"
-            class="accent"
-            >
-              <v-card-title>
-               Create proposal
-              </v-card-title>
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="12"
-                    >
-                      <v-text-field
-                        v-model="title"
-                        label="Title proposal"
-                        required
-                        outlined
-                      ></v-text-field>
-                    </v-col>
-
-                    <!--<v-col cols="12">
-                      <v-text-field
-                        v-model="selectPolicy"
-                        label="Policy address"
-                        disabled
-                        outlined
-                      ></v-text-field>
-                    </v-col>  v-model="accounts[0].address"-->
-                    <v-col cols="12">
-                      <v-text-field
-                        v-model="accounts[0].address"
-                        label="Proposers"
-                        required
-                        outlined
-                        disabled
-                      ></v-text-field>
-                    </v-col>
-                    <!--<v-col cols="12">
-                      <v-text-field
-                        label="Metadata"
-                        required
-                        outlined
-                      ></v-text-field>
-                    </v-col>-->
-                    <v-col cols="12">
-                      <!--<v-combobox
-                        v-model="selectMsgType"
-                        :items="items"
-                        label="Select message"
-                        multiple
-                        chips
-                        outlined
-                        solo
-                      ></v-combobox>-->
-                      <v-select
-                        v-model="selectMsgType"
-                        :items="items"
-                        label="Select"
-                        multiple
-                        chips
-                        outlined
-                        hint="Select type action of sdk"
-                        persistent-hint
-                      ></v-select>
-                    </v-col>
-                  </v-row>
-                </v-container>
-
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-               <!-- <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="dialog = false"
-                >
-                  Close
-                </v-btn>-->
-                <v-btn
-                  class="accent"
-                  @click="createProposal"
-                >
-                  Create proposal
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-          <v-col cols="6">
-            <v-card
-              class="accent"
-            >
-
-              <v-card-title>
-               Messages type
-
-
-              </v-card-title>
-              <v-card-text>
-                <v-expansion-panels focusable>
-                  <v-expansion-panel
-                    v-for="(item,i) in selectMsgType"
-                    :key="i"
-                    class="accent"
-                  >
-                    <v-expansion-panel-header>{{ item }}</v-expansion-panel-header>
-                    <v-expansion-panel-content v-if="item === 'Text Proposal'">
-                      <messagesTypeTextProposal :from="selectPolicy" />
-                    </v-expansion-panel-content>
-                    <v-expansion-panel-content v-if="item === 'Delegate'">
-                      <messagesTypeDelegateTx :from="selectPolicy" />
-                    </v-expansion-panel-content>
-                    <v-expansion-panel-content v-if="item === 'Create Validator'">
-                      <messagesTypeCreateVal :from="selectPolicy" />
-                    </v-expansion-panel-content>
-                    <v-expansion-panel-content v-if="item === 'CosmWasm'">
-                      <messagesTypeCosmwasmTx :from="selectPolicy" />
-                    </v-expansion-panel-content>
-                  </v-expansion-panel>
-                </v-expansion-panels>
-
-
-
-                <!--{{ selectMsgType }}-->
-              </v-card-text>
-
-            </v-card>
-          </v-col>
-        </v-row>
-
-
-      </v-col>          
-          </v-card-text>
-        </v-card>
-      </v-tab-item>      
-    </v-tabs-items>
-  </v-card>
-  </v-col>
-
-    </v-row>
+  </v-row>
 </template>
 <script>
 import { mapState } from 'vuex'
 import axios from 'axios'
 import { coins } from "@cosmjs/launchpad";
-import { SigningStargateClient, AminoTypes } from "@cosmjs/stargate";
-import { Registry, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { SigningStargateClient, AminoTypes, defaultRegistryTypes, GasPrice, assertIsDeliverTxSuccess } from "@cosmjs/stargate";
+import { Registry } from "@cosmjs/proto-signing";
 import { cosmos, cosmosProtoRegistry, cosmosAminoConverters } from 'interchain46';
+import { Any } from "cosmjs-types/google/protobuf/any";
+import { TextProposal } from "cosmjs-types/cosmos/gov/v1beta1/gov";
 import pkg from 'protobufjs';
 const { Type, Field } = pkg;
 import cosmosConfig from '~/cosmos.config'
@@ -264,18 +76,6 @@ import { notifWaiting, notifError, notifSuccess } from '~/libs/notifications'
 
 export default {
   data: () => ({
-    title: '',
-    selectPolicy: '',
-    selectMsgType: '',
-    finalGroupData: '',
-    allProps: [],
-    allExec: [],
-    allVotes: [],
-    search: '',
-    columnName:'id',
-    isDescending:true,
-    allPolicy: [],
-    tab: null,
     items: [
       'Text Proposal',
       //'Parameter Change Proposal',
@@ -286,106 +86,74 @@ export default {
       //'Create Validator',
       //'CosmWasm',
     ],
-
+    // New
+    propText: '',
+    propTitle: '',
+    proposer: '',
+    initDeposit: '4200000',
+    isSend: false,
   }),
   computed: {
     ...mapState('keplr', [`accounts`, `initialized`, `error`, `logged`, `logout`]),
-    ...mapState('data', [`myGroup`, 'infoGroupId', 'policyGroupId', 'finalMsgProp'])
+    ...mapState('data', ['chainId'])
   },
   watch: {
-    // whenever question changes, this function will run
-    selectPolicy(newQuestion, oldQuestion) {
-      // console.log(newQuestion)
-    }
+
   },
-  async mounted () {
-
-//     if (!this.logged) {
-//       this.$router.push({path: "/login"})
-//       return
-//     }
-
-const {
-    deposit,
-    submitProposal,
-    vote,
-    voteWeighted
-} = cosmos.gov.v1.MessageComposer.fromPartial;
-
-const registry = new Registry(cosmosProtoRegistry);
-
-console.log(registry)
-
+  async mounted() {
+    this.isSend = false
+    this.proposer = this.accounts[0].address
   },
   methods: {
-      async createProposal() {
+    async createProposalv1Beta() {
 
-        var returnWaiting = notifWaiting(this.$toast)
+      const chainId = cosmosConfig[this.chainId].chainId;
+      await window.keplr.enable(chainId);
+      const offlineSigner = await window.getOfflineSignerAuto(chainId);
 
+      const client = await SigningStargateClient.connectWithSigner(
+        cosmosConfig[this.chainId].rpcURL,
+        offlineSigner,
+        { gasPrice: GasPrice.fromString(cosmosConfig[this.chainId].gasPrice + cosmosConfig[this.chainId].coinLookup.chainDenom) }
+      )
 
-        const registry = new Registry(cosmosProtoRegistry);
-        const aminoTypes = new AminoTypes({ ...cosmosAminoConverters })
+      const foundMsgType = defaultRegistryTypes.find(element => element[0] === '/cosmos.gov.v1beta1.MsgSubmitProposal');
+      const textProposal = TextProposal.fromPartial({
+        title: this.propTitle,
+        description: this.propText,
+      });
+      const initialDeposit = coins(this.initDeposit, "ubcna");
 
-        const chainId = cosmosConfig[0].chainId;
-        await window.keplr.enable(chainId);
-        const offlineSigner = await window.getOfflineSignerAuto(chainId);
-        const accounts = await offlineSigner.getAccounts();
+      const finalMsg = {
 
-        const client = await SigningStargateClient.connectWithSigner(
-          cosmosConfig[0].rpcURL,
-          offlineSigner,
-          { registry: registry, aminoTypes: aminoTypes  }
-        )
-
-        const { submitProposal } = cosmos.gov.v1.MessageComposer.withTypeUrl;
-        console.log(this.finalMsgProp)
-
-        const msg = submitProposal({
-            messages: this.finalMsgProp,
-            initialDeposit: [],
-            proposer: accounts[0].address,
-            metadata: ''
+        typeUrl: foundMsgType[0],
+        value: foundMsgType[1].fromPartial({
+          content: Any.fromPartial({
+            typeUrl: "/cosmos.gov.v1beta1.TextProposal",
+            value: Uint8Array.from(TextProposal.encode(textProposal).finish()),
+          }),
+          proposer: this.accounts[0].address,
+          initialDeposit: initialDeposit,
         })
-//         const fee = {
-//           amount: coins(200, "udao"),
-//           gas: "200000",
-//         };
-        const fee = {
-          amount: coins(200, "utoken"),
-          gas: "200000",
-        };
-           /* const fee = {
-              amount: [
-                {
-                  denom: cosmosConfig[0].coinLookup.chainDenom,
-                  amount: '5000',
-                },
-              ],
-              gas: '200000',
-            } */
-        console.log(msg)
-
-        try {
-          const result = await client.signAndBroadcast(accounts[0].address, [msg], fee)
-          console.log(result)
-
-          //this.$toast.dismiss(returnWaiting);
-          // Send notification
-          // notifSuccess(this.$toast, result.transactionHash)
-          // await this.$store.dispatch('data/getMyGroup', accounts[0].address)
-        } catch (e) {
-            console.error(e);
-            this.$toast.dismiss(returnWaiting);
-            notifError(this.$toast)
-        }
-
 
       }
+      console.log(finalMsg)
 
+      try {
+        const proposalResult = await client.signAndBroadcast(
+          this.accounts[0].address,
+          [finalMsg],
+          'auto',
+          "Test proposal from ui",
+        );
+        assertIsDeliverTxSuccess(proposalResult);
+        console.log(proposalResult)
+        this.isSend = true
+      } catch (e) {
+        console.error(e);
+      }
+    },
   }
 }
 </script>
-<style>
-
-
-</style>
+<style></style>
